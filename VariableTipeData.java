7ae3deef9171d4fe9b4e3f47c6df10ee0bf90f3d
Meiriskaagustina12 <114629700+Meiriskaagustina12@users.noreply.github.com>
2023-02-23 11:16:33 +0700

    Add files via upload

---
+++ VariableTipeData.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+//PIC -> Package, Import, Class
+
+public class VariableTipeData {
+    
+    static String nama;
+    static int nilai;
+    static Integer sks;
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Isikan Nama:");
+        nama = sc.nextLine();
+        
+        System.out.println("Nama anda :" + nama);
+    }
+}
